4f6ad7a4e4f38a3b43111f92813a3fcec88b7e0b
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-07-18 16:26:19 +0200

    Configuration
    Mise en place de tout le nécessaire pour la persistence
    
    le Pom
    le persistence.xml / non commit
    Test de connection OK

--- src/main/java/diginamic/App.java
+++ src/main/java/diginamic/App.java
@@ -1,5 +1,10 @@
 package diginamic;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        EntityManagerFactory emf  = Persistence.createEntityManagerFactory("pu_petstore");
+        EntityManager em = emf.createEntityManager();
+        //EntityTransaction tr = em.getTransaction();
+        
+       
+        em.close();
+        emf.close();
+        
+        
+        
+        
+        
     }
 }
--- src/test/java/diginamic/AppTest.java
+++ src/test/java/diginamic/AppTest.java
@@ -2,19 +2,23 @@ package diginamic;
 
 import static org.junit.Assert.assertTrue;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Test
+	public void TestConnection() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu_petstore");
+		EntityManager em = emf.createEntityManager();
+		assertTrue(em.isOpen());
+		emf.close();
+		em.close();
+	}
 }
